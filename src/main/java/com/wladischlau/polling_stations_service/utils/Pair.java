5bdf0e6e00c534918d18ed121efee4b24247cc91
Vladislav Nasevich <91089134+watisluv1910@users.noreply.github.com>
2024-04-23 14:35:19 +0300

    fix: remove unused Pair class

--- src/main/java/com/wladischlau/polling_stations_service/utils/Pair.java
+++
@@ -1,8 +0,0 @@
-package com.wladischlau.polling_stations_service.utils;
-
-public record Pair<T, S>(T first, S second) {
-
-    public String toString() {
-        return "(" + first + "," + second + ")";
-    }
-}
