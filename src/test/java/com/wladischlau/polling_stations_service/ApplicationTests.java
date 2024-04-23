04c77f5fb6ef81aa5454c3092522d35d5cc3a05c
Vladislav Nasevich <91089134+watisluv1910@users.noreply.github.com>
2024-04-23 03:01:49 +0300

    feat: initial commit

---
+++ src/test/java/com/wladischlau/polling_stations_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wladischlau.polling_stations_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
